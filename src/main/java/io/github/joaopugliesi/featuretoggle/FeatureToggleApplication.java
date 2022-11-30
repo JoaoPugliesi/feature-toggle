bdd9ad13afbfab6c7152d37895832036269cf52b
Joao Pugliesi <joaopaulo.allex@gmail.com>
2022-11-29 22:15:55 -0300

    First project commit

---
+++ src/main/java/io/github/joaopugliesi/featuretoggle/FeatureToggleApplication.java
@@ -0,0 +1,13 @@
+package io.github.joaopugliesi.featuretoggle;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FeatureToggleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FeatureToggleApplication.class, args);
+	}
+
+}
